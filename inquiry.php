d8bead2c8a1f47951dacadc17ca79de0fafdfd62
Stsmecical <gaurav.stsmedical@gmail.com>
2024-10-22 12:33:48 +0530

    Create inquiry.php

---
+++ inquiry.php
@@ -0,0 +1,61 @@
+<?php 
+
+// Google Recaptcha v2
+     if(isset($_POST['g-recaptcha-response']))
+        $captcha=$_POST['g-recaptcha-response'];
+
+        $ip=$_SERVER['REMOTE_ADDR'];
+
+        if($recaptcha) {
+        $recaptcha.setAttribute("required", "required");
+        }
+    
+        $secretkey="6LfiAXEgAAAAAP-pQHhzIF4nrdJdawdh26KDITFz";
+        $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=".$captcha."&remoteip=".$ip), true);
+        if($response['success'] == false)
+        {
+          // echo '<h2>You are spammer ! Get the @$%K out</h2>';
+          echo"<script>alert('Please fill the captcha code'); window.location='index.php'</script>";
+        }
+        else
+        { 
+   
+    // mail code
+
+$name=$_POST["name"];
+$email=$_POST["email"];
+$phone=$_POST["phone"];
+$message=$_POST["message"];
+
+date_default_timezone_set("Asia/Calcutta");   //India time (GMT+5:30)
+$da= date('h:i:s a');
+
+$rndno='<b>Name: </b>'.$name."<br>
+<b>Email: </b>".$email."<br>
+<b>Phone: </b>".$phone."<br>
+<b>Message: </b>".$message;   
+ 
+$to="info@stsmedicals.com, kaleemwxi@gmail.com";
+
+$kam="<i class='fa fa-clock-o' style='font-size:20px'></i>";
+
+$subject = 'Website Enquiry - stsmedicals.com '.  $da;
+
+$txt = "<h3><b>Contact Enquiry form stsmedicals.com</b></h3> ".$rndno; 
+$headers = "MIME-Version: 1.0" . "\r\n";
+
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+$headers .= 'From: stsmedicals.com '. $name  . '<info@stsmedicals.com>' . "\r\n";
+
+  mail($to,$subject,$txt,$headers);
+       
+  #echo"<script>alert('Your mail has been sent.'); window.location='index.php'</script>";
+ 
+   header('Location: thanks.php');
+  //echo 'Message has been sent';
+  
+exit;
+           // echo "success";
+      }
+?>
